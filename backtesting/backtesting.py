[UPDATE] correct spread calculation and execution

backtesting/backtesting.py
@@ -558,6 +558,8 @@ class Trade:
         self._commissions = 0
         self._risk_dollars = 0
         self._exit_reason = "MANUALLY"
+        self._entry_spread = 0.0
+        self._exit_spread = 0.0
 
     def __repr__(self):
         return f'<Trade size={self.__size} time={self.__entry_bar}-{self.__exit_bar or ""} ' \
@@ -722,17 +724,44 @@ class Trade:
     def tp(self, price: float):
         self.__set_contingent('tp', price)
 
+    # def __set_contingent(self, type, price):
+    #     assert type in ('sl', 'tp')
+    #     assert price is None or 0 < price < np.inf, f'Make sure 0 < price < inf! price: {price}'
+    #     attr = f'_{self.__class__.__qualname__}__{type}_order'
+    #     order: Order = getattr(self, attr)
+    #     if order:
+    #         order.cancel()
+    #     if price:
+    #         kwargs = {'stop': price} if type == 'sl' else {'limit': price}
+    #         order = self.__broker.new_order(-self.size, trade=self, tag=self.tag, **kwargs)
+    #         setattr(self, attr, order)
+
     def __set_contingent(self, type, price):
-        assert type in ('sl', 'tp')
-        assert price is None or 0 < price < np.inf, f'Make sure 0 < price < inf! price: {price}'
-        attr = f'_{self.__class__.__qualname__}__{type}_order'
-        order: Order = getattr(self, attr)
+        assert type in ("sl", "tp")
+        assert price is None or 0 < price < np.inf, f"Make sure 0 < price < inf! price: {price}"
+
+        if type == "sl":
+            order = self.__sl_order
+        else:
+            order = self.__tp_order
+
         if order:
             order.cancel()
+
         if price:
-            kwargs = {'stop': price} if type == 'sl' else {'limit': price}
-            order = self.__broker.new_order(-self.size, trade=self, tag=self.tag, **kwargs)
-            setattr(self, attr, order)
+            kwargs = {"stop": price} if type == "sl" else {"limit": price}
+            order = self.__broker.new_order(
+                -self.size,
+                trade=self,
+                tag=self.tag,
+                **kwargs
+            )
+
+            if type == "sl":
+                self.__sl_order = order
+            else:
+                self.__tp_order = order
+
 
 
 class _Broker:
@@ -829,12 +858,25 @@ class _Broker:
         """ Price at the last (current) close. """
         return self._data.Close[-1]
 
-    def _adjusted_price(self, size=None, price=None) -> float:
-        """
-        Long/short `price`, adjusted for spread.
-        In long positions, the adjusted price is a fraction higher, and vice versa.
-        """
-        return (price or self.last_price) * (1 + copysign(self._spread, size))
+    # def _adjusted_price(self, size=None, price=None) -> float:
+    #     """
+    #     Long/short `price`, adjusted for spread.
+    #     In long positions, the adjusted price is a fraction higher, and vice versa.
+    #     """
+    #     return (price or self.last_price) * (1 + copysign(self._spread, size))
+    
+    def _adjusted_price(self, size, price=None, i=None):
+        if i is None:
+            i = self._i
+
+        # spread = self._data.Spread[i]
+
+        # Long → Ask, Short → Bid
+        if size > 0:
+            return self._data.Ask[i]
+        else:
+            return self._data.Bid[i]
+
 
     @property
     def equity(self) -> float:
@@ -1054,7 +1096,8 @@ class _Broker:
 
             # Open a new trade
             if need_size:
-                self._open_trade(adjusted_price,
+                entry_price = self._adjusted_price(need_size, price, time_index)
+                self._open_trade(entry_price,
                                  need_size,
                                  order.sl,
                                  order.tp,
@@ -1124,8 +1167,16 @@ class _Broker:
 
         trade._exit_reason = exit_reason
         
-        closed_trade = trade._replace(exit_price=price, exit_bar=time_index)
+        # closed_trade = trade._replace(exit_price=price, exit_bar=time_index)
+        exit_price = self._adjusted_price(-trade.size, price, time_index)
+
+        closed_trade = trade._replace(
+            exit_price=exit_price,
+            exit_bar=time_index
+        )
+
         closed_trade._exit_reason = exit_reason
+        closed_trade._exit_spread = self._data.Spread[time_index]
         if round(abs(closed_trade.r), 3) == 0:
             trade._exit_reason = "BE"
         self.closed_trades.append(closed_trade)
@@ -1142,6 +1193,7 @@ class _Broker:
                     sl: Optional[float], tp: Optional[float], time_index: int, tag, risk_dollars):
         trade = Trade(self, size, price, time_index, tag)
         trade._risk_dollars = risk_dollars
+        trade._entry_spread = self._data.Spread[time_index]
         self.trades.append(trade)
         # Apply broker commission at trade open
         self._cash -= self._commission(size, price)